605b74382b6feb135e116bd1d3033bc400d687d1
jrmullaney <j.mullaney@sheffield.ac.uk>
2018-07-25 13:05:16 +0100

    Implementing proper compression

--- config/makeSkyMap.py
+++ config/makeSkyMap.py
@@ -5,10 +5,11 @@ obj = printDict(config, path=['config'])
 config.skyMap = "healpix"
 
 # Configuration for HealpixSkyMap
-config.skyMap["healpix"].log2NSide = 3
+config.skyMap["healpix"].log2NSide = 2
 config.skyMap["healpix"].pixelScale = 1.2
-config.skyMap["healpix"].patchInnerDimensions = (6000, 8000)
+config.skyMap["healpix"].patchInnerDimensions = (8000, 8000)
 config.skyMap["healpix"].patchBorder = 100 # Pixels
-config.skyMap["healpix"].tractOverlap = 1. # Degrees
+config.skyMap["healpix"].tractOverlap = 0.1 # Degrees
+config.skyMap["healpix"].rotation = 45.
 config.skyMap["healpix"].projection = "TAN"
 
