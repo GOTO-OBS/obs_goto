879c9384daf8837d6d18baefa9c58bf0d73203d0
jrmullaney <j.mullaney@sheffield.ac.uk>
2018-09-24 13:28:52 +0100

    Updated injectFakes for new frames

--- bin.src/injectFakes.py
+++ bin.src/injectFakes.py
@@ -5,10 +5,10 @@ import lsst.afw.display.ds9 as ds9
 import numpy as np
 from astropy.io import fits
 
-DATA_DIR = "/local/ph1jxm/GOTO/real/DATA/rerun/outSF"
+DATA_DIR = "/local2/ph1jxm/GOTO/processedDataV16/DATA/rerun/outSF"
 butler = dafPersistence.Butler(DATA_DIR)
 
-calexp = butler.get("calexp", visit=18021, ccd=2, immediate=True)
+calexp = butler.get("calexp", visit=54479, ccd=4, immediate=True)
 
 np.random.seed(1)
 nstars = 500
@@ -25,9 +25,9 @@ for x, y, mag in zip(xs,ys,mags):
     x1, y1 = 44+int(x)+8, 64+int(y)+8
     srcarr[y0:y1,x0:x1] = srcarr[y0:y1,x0:x1] + flux*psf
 
-hdu = fits.open("/local/ph1jxm/GOTO/real/raw/r0018021_UT2.fits")
+hdu = fits.open("/local2/ph1jxm/GOTO/rawData/2018-09-19/r0054479_UT4.fits")
 hdu[1].data = hdu[1].data + srcarr
-hdu[1].header['RUN-ID'] = 'r1018021'
-#hdu.writeto("/local/ph1jxm/GOTO/real/raw/r1018021_UT2.fits", overwrite=True)
+hdu[1].header['RUN-ID'] = 'r1054479'
+hdu.writeto("/local2/ph1jxm/GOTO/rawData/2018-09-19/r1054479_UT4.fits", overwrite=True)
 
 
