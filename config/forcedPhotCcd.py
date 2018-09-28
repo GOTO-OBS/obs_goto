ca5c977f9a7093d093a557ebee73bbc446ab538a
jrmullaney <j.mullaney@sheffield.ac.uk>
2018-09-28 16:27:26 +0100

    Forced photometry working; need to select measurements to speed up

--- config/forcedPhotCcd.py
+++ config/forcedPhotCcd.py
@@ -1,3 +1,27 @@
-from lsst.obs.goto.printDict  import printDict
-#obj = printDict(config, path=['config'])
-#quit()
+for i in [
+        'base_PsfFlux',
+        'base_PeakLikelihoodFlux',
+        'base_GaussianFlux',
+        'base_SdssShape', #base_SdssShape is needed for PSF determination.
+        'base_ScaledApertureFlux',
+        'base_CircularApertureFlux',
+        #'base_TransformedCentroid',
+        #'base_TransformedShape',
+        #'base_Blendedness',
+        #'base_LocalBackground',
+        #'base_Variance',
+        #'base_InputCount',
+         ]:
+    config.measurement.plugins[i].doMeasure=False
+    config.measurement.undeblended[i].doMeasure=False
+    
+config.measurement.plugins['base_CircularApertureFlux'].radii=[6.0, 9.0, 12.0]
+config.measurement.undeblended['base_CircularApertureFlux'].radii=[6.0, 9.0, 12.0]
+
+config.measurement.plugins.names=['base_SdssCentroid',
+                                  'base_TransformedCentroid',
+                                  'base_CircularApertureFlux',
+                                  'base_TransformedShape']
+
+#Currently breaks when doing aperture corrections; fix this.
+config.doApCorr=False
