9b0da78cae22d74f538e0bb70a64f4edab19c1e5
jrmullaney <j.mullaney@sheffield.ac.uk>
2018-10-01 17:26:10 +0100

    Selected minimal forced photometry types to speed up that stage

--- config/forcedPhotCcd.py
+++ config/forcedPhotCcd.py
@@ -1,16 +1,18 @@
 for i in [
-        'base_PsfFlux',
-        'base_PeakLikelihoodFlux',
-        'base_GaussianFlux',
-        'base_SdssShape', #base_SdssShape is needed for PSF determination.
-        'base_ScaledApertureFlux',
-        'base_CircularApertureFlux',
-        #'base_TransformedCentroid',
-        #'base_TransformedShape',
-        #'base_Blendedness',
-        #'base_LocalBackground',
-        #'base_Variance',
-        #'base_InputCount',
+        'base_PsfFlux',#Not needed
+        'base_PeakLikelihoodFlux',#Not needed
+        'base_GaussianFlux',#Not needed
+        'base_NaiveCentroid',
+        'base_SdssCentroid',
+        'base_SdssShape', #Not needed
+        'base_ScaledApertureFlux',#Not needed
+        #'base_CircularApertureFlux',
+#        'base_TransformedCentroid',
+#        'base_TransformedShape',
+        'base_Blendedness', #Not needed
+        'base_LocalBackground', #Not needed
+        'base_Variance', #Not needed
+        'base_InputCount', #Not needed
          ]:
     config.measurement.plugins[i].doMeasure=False
     config.measurement.undeblended[i].doMeasure=False
@@ -18,10 +20,12 @@ for i in [
 config.measurement.plugins['base_CircularApertureFlux'].radii=[6.0, 9.0, 12.0]
 config.measurement.undeblended['base_CircularApertureFlux'].radii=[6.0, 9.0, 12.0]
 
-config.measurement.plugins.names=['base_SdssCentroid',
-                                  'base_TransformedCentroid',
+config.measurement.plugins.names=['base_TransformedCentroid',
                                   'base_CircularApertureFlux',
                                   'base_TransformedShape']
 
 #Currently breaks when doing aperture corrections; fix this.
-config.doApCorr=False
+config.doApCorr=True
+
+#Forced measurement currently takes about 5.5 minutes. Why is it so slow compared to normal measurement?...Because it has to warp the shapes of the apertures to match the WCS
+
