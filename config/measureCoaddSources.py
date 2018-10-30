9e377968bb520abaa6b3cea0972ad38399e3cee2
jrmullaney <j.mullaney@sheffield.ac.uk>
2018-10-30 10:37:49 +0000

    Lydia's changes to assembleCoadd and config/measureCoaddSources configs

--- config/measureCoaddSources.py
+++ config/measureCoaddSources.py
@@ -8,8 +8,11 @@ from lsst.utils import getPackageDir
 print('***Lydia***: config/measureCoaddSources.py:' \
       'Setting doPropagateFlags to False. This could cause problems')
 config.doPropagateFlags=False
-for source, target in [('B', 'b'), ('G', 'v'), ('R', 'm'), ('L', 'v')]:
+config.match.refObjLoader.ref_dataset_name = "ps1_pv3_3pi_20170110"
+for source, target in [('B', 'g'), ('G', 'g'), ('R', 'r'), ('L', 'g')]:
     config.match.refObjLoader.filterMap[source]=target
 #config.load(os.path.join(getPackageDir("obs_goto"), "config", "cmodel.py"))
 #quit()
 config.doMatchSources=False
+config.doWriteMatchesDenormalized = True
+config.match.matchRadius=1.0
