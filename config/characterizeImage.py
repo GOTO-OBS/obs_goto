46112ddbfac147988a8a1321dd8c59f5ab5ae5b2
jrmullaney <j.mullaney@sheffield.ac.uk>
2018-11-29 21:41:16 +0000

    Re-wrote gotoCharTask to minimise the number of detection and measurement rounds. Speeds up processCcd considerably

--- config/characterizeImage.py
+++ config/characterizeImage.py
@@ -1,23 +1,11 @@
-#from lsst.obs.goto.gotoNullIsr import GotoNullIsrTask
-import os.path
-from lsst.utils import getPackageDir
 from lsst.meas.algorithms import LoadIndexedReferenceObjectsTask
 
-configDir = os.path.join(getPackageDir("obs_goto"), "config")
-#Retarget the characterize image task to my own in gotoCharTask.
-#Our own performs (almost) blind astrometry with aNet straight after ISR.
-#from lsst.obs.goto.gotoCharTask import GotoCharacterizeImageTask
-#config.retarget(GotoCharacterizeImageTask)
-config.measurement.slots.calibFlux='base_CircularApertureFlux_9_0'
-config.measureApCorr.refFluxName='base_CircularApertureFlux_6_0'
-
-config.doWriteExposure=True
-config.doEarlyAstrometry=True
-config.earlyAstrometry.detection.includeThresholdMultiplier = 20.0
-#config.earlyAstrometry.astromRefObjLoader.filterMap = {'L':'v'}
-for source, target in [('B', 'b'), ('G', 'v'), ('R', 'm'), ('L', 'v')]:
-    config.earlyAstrometry.astromRefObjLoader.filterMap[source]=target
-config.earlyAstrometry.detection.minPixels = 5
+config.detection.minPixels = 20
+config.detection.thresholdValue = 5.0
+config.detection.includeThresholdMultiplier = 20.0
+config.detection.minPixels = 5
+config.detection.doTempLocalBackground=True
+config.detection.tempLocalBackground.binSize = 32
 
 #Don't make lots of measurements if we can avoid it:
 #Dew needed for earlyAstrometry:
@@ -25,7 +13,7 @@ for i in [
 #        'base_GaussianFlux',
 #        'base_SdssShape', #base_SdssShape is needed for PSF determination.
         'base_ScaledApertureFlux',
-        'base_CircularApertureFlux',
+#        'base_CircularApertureFlux',
         'base_Blendedness',
         'base_LocalBackground',
         'base_Jacobian',
@@ -33,66 +21,40 @@ for i in [
         'base_Variance',
         'base_InputCount',
         'base_SkyCoord']:
-    config.earlyAstrometry.measurement.plugins[i].doMeasure=False
-
-#But a few more needed for measurePsf:
-#config.charImage.measurement.plugins[i].doMeasure=False
-
+    config.measurement.plugins[i].doMeasure=False
+config.measurement.slots.calibFlux='base_CircularApertureFlux_9_0'
+    
+config.doEarlyAstrometry=True
 from lsst.meas.extensions.astrometryNet import ANetAstrometryTask
-config.earlyAstrometry.astrometry.retarget(ANetAstrometryTask)
-config.earlyAstrometry.astrometry.solver.useWcsRaDecCenter = True
-config.earlyAstrometry.astrometry.solver.useWcsParity = True
-config.earlyAstrometry.astrometry.solver.useWcsPixelScale = True
-config.earlyAstrometry.astrometry.solver.raDecSearchRadius = 5.
-config.earlyAstrometry.astrometry.solver.maxStars = 500
-config.earlyAstrometry.astrometry.solver.catalogMatchDist = 10.
-config.earlyAstrometry.astrometry.solver.pixelScaleUncertainty= 1.1
-#config.earlyAstrometry.astrometry.solver.filterMap = {'L':'v'}
+config.astrometry.retarget(ANetAstrometryTask)
 for source, target in [('B', 'b'), ('G', 'v'), ('R', 'm'), ('L', 'v')]:
-    config.earlyAstrometry.astrometry.solver.filterMap[source]=target
-config.earlyAstrometry.astrometry.solver.pixelMargin = 50
-config.earlyAstrometry.astrometry.solver.sipOrder = 3
-config.earlyAstrometry.astrometry.solver.calculateSip = True
-config.earlyAstrometry.astrometry.solver.maxIter = 20
-config.earlyAstrometry.astrometry.solver.matchDistanceSigma = 2.0
-config.earlyAstrometry.astrometry.solver.cleaningParameter = 5.0
-
-config.earlyAstrometry.astrometry.rejectThresh = 3.0
-
-config.detection.thresholdValue = 5.0
-config.detection.includeThresholdMultiplier = 10.0
-config.detection.minPixels = 20
+    config.astromRefObjLoader.filterMap[source]=target
+    config.astrometry.solver.filterMap[source]=target
+config.astrometry.solver.useWcsRaDecCenter = True
+config.astrometry.solver.useWcsParity = True
+config.astrometry.solver.useWcsPixelScale = True
+config.astrometry.solver.raDecSearchRadius = 5.
+config.astrometry.solver.maxStars = 500
+config.astrometry.solver.catalogMatchDist = 10.
+config.astrometry.solver.pixelScaleUncertainty= 1.1
+config.astrometry.solver.pixelMargin = 50
+config.astrometry.solver.sipOrder = 3
+config.astrometry.solver.calculateSip = True
+config.astrometry.solver.maxIter = 20
+config.astrometry.solver.matchDistanceSigma = 2.0
+config.astrometry.solver.cleaningParameter = 5.0
+config.astrometry.rejectThresh = 3.0
 
 #Cosmic rays and background estimation:
 config.repair.doCosmicRay = True
 config.repair.cosmicray.nCrPixelMax=1000000
 config.repair.cosmicray.cond3_fac2 = 0.4
-config.detection.doTempLocalBackground=True
-config.detection.tempLocalBackground.binSize = 32
 
 #PSF determination:
 config.psfIterations=1
-#config.measurePsf.reserveFraction = 0.2
-#config.measurePsf.starSelector["objectSize"].sourceFluxField = 'base_PsfFlux_flux'
-
-#config.measurePsf.starSelector['objectSize'].widthMin=1.0
-#config.measurePsf.starSelector['objectSize'].widthMax=4.0
-#config.measurePsf.starSelector['secondMoment'].clumpNSigma=3.0
 config.measurePsf.starSelector["objectSize"].fluxMin = 1000.
 config.measurePsf.starSelector['objectSize'].widthStdAllowed = 10.0
 config.measurePsf.starSelector['objectSize'].nSigmaClip = 10.0
-
-#try:
-#    import lsst.meas.extensions.psfex.psfexPsfDeterminer
-    #import lsst.meas.extensions.psfex.psfexStarSelector
-#    config.measurePsf.psfDeterminer["psfex"].spatialOrder = 2
-#    config.measurePsf.psfDeterminer["psfex"].psfexBasis = 'PIXEL_AUTO'
-#    config.measurePsf.psfDeterminer["psfex"].samplingSize = 0.7
-#    config.measurePsf.psfDeterminer["psfex"].kernelSize = 21
-#    config.measurePsf.psfDeterminer.name = "psfex"
-#except ImportError as e:
-#    print("WARNING: Unable to use psfex: %s" % e)
-
 config.measurePsf.psfDeterminer.name = "pca"
 config.measurePsf.psfDeterminer['pca'].nEigenComponents = 6
 config.measurePsf.psfDeterminer['pca'].spatialOrder = 6
@@ -101,24 +63,6 @@ config.measurePsf.psfDeterminer['pca'].sizeCellY = 512
 config.measurePsf.psfDeterminer['pca'].reducedChi2ForPsfCandidates = 10.0
 
 config.doApCorr = True
+config.measureApCorr.refFluxName='base_CircularApertureFlux_6_0'
 
-#config.refObjLoader.defaultFilter ='m'
-
-# Activate calibration of measurements: required for aperture corrections
-#config.load(os.path.join(configDir, "cmodel.py"))
-#config.measurement.load(os.path.join(configDir, "apertures.py"))
-#config.measurement.load(os.path.join(configDir, "kron.py"))
-#config.measurement.load(os.path.join(configDir, "convolvedFluxes.py"))
-
-#config.detection.includeThresholdMultiplier = 3.0
-
-#config.measurement.plugins['modelfit_CModel'].region.badMaskPlanes=['EDGE', 'SAT', 'BAD', 'NO_DATA']
-#config.measurement.undeblended['modelfit_CModel'].region.badMaskPlanes=['EDGE', 'SAT', 'BAD', 'NO_DATA']
-#config.measurement.undeblended['modelfit_CModel'].region.maxBadPixelFraction=None
-#config.measurement.plugins['modelfit_CModel'].region.maxBadPixelFraction=None
-
-
-
-#config.measureApCorr.starSelector['psfex'].maxbadflag=False
-#config.measureApCorr.starSelector['psfex'].badFlags=[]
-#config.measurePsf.psfDeterminer['psfex'].badMaskBits=[]
+config.doWrite=False
