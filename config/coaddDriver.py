c86b9e9e7adb3920f0d2029cde6ae12a210c0984
jrmullaney <j.mullaney@sheffield.ac.uk>
2018-12-07 20:44:50 +0000

    Neatened config/coaddDriver.py

--- config/coaddDriver.py
+++ config/coaddDriver.py
@@ -1,15 +1,10 @@
-from lsst.obs.goto.printDict  import printDict
-from lsst.pipe.tasks.selectImages import PsfWcsSelectImagesTask
+#This is needed as we use 'run' instead of 'visit':
 from lsst.obs.goto.makeGotoCoaddTempExp import GotoMakeCoaddTempExpTask
-
 config.makeCoaddTempExp.retarget(GotoMakeCoaddTempExpTask)
+
+#Uncommentn this if we want to select frames based on PSF:
+#from lsst.pipe.tasks.selectImages import PsfWcsSelectImagesTask
 #config.assembleCoadd.select.retarget(PsfWcsSelectImagesTask)
-#config.select.retarget(PsfWcsSelectImagesTask)
-
-#config.detectCoaddSources.detection.background.binSize=4000
-
-#config.makeCoaddTempExp.makePsfMatched = True
-#config.makeCoaddTempExp.doPsfMatch = True
 
 #Detection can be slow, especially if measuring a lot of parameters.
 #Can turn on when creating deep catalogues.
