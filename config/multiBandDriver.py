b46001feec3ca696bbe582081f4d5a9696cdfc2f
jrmullaney <j.mullaney@sheffield.ac.uk>
2018-12-07 22:08:55 +0000

    Minimized measurements in MBD

--- config/multiBandDriver.py
+++ config/multiBandDriver.py
@@ -11,22 +11,23 @@ for sub in ("mergeCoaddDetections",
         getattr(config, sub).load(path)
 
 config.doDetection=True
+config.detectCoaddSources.detection.retarget(SourceDetectionTask)
 config.detectCoaddSources.doScaleVariance=True
 config.detectCoaddSources.detection.doTempWideBackground=False
 
-for i in [
-        #'base_GaussianFlux', Needed for PSF in imageDifference.py
-        'base_SdssShape',
-        'base_ScaledApertureFlux',
-        #'base_CircularApertureFlux', Needed for zeropoint
-        'base_Blendedness',
-        'base_NaiveCentroid',
-        'base_LocalBackground',
-        'base_Jacobian',
-        'base_FPPosition',
-        'base_Variance',
-        'base_InputCount',
-        #'base_SkyCoord', Needed for zeropoint
+for i in [#'base_PixelFlags',
+          #'base_SdssCentroid',#Needed for positions
+          'base_NaiveCentroid',
+          'base_SdssShape',
+          'base_GaussianFlux', #Needed for PSF in imageDifference.py
+          'base_PsfFlux',
+          #'base_CircularApertureFlux',
+          #'base_SkyCoord',
+          'base_Variance',
+          'base_Blendedness',
+          'base_LocalBackground',
+          'base_ScaledApertureFlux',
+          'base_InputCount',
 ]:
     config.measureCoaddSources.measurement.plugins[i].doMeasure=False
     config.forcedPhotCoadd.measurement.plugins[i].doMeasure=False
