e39a013a42bba521cce6068155dd063edc3c240f
jrmullaney <j.mullaney@sheffield.ac.uk>
2018-12-07 20:55:27 +0000

    Added new isr.py

---
+++ config/isr.py
@@ -0,0 +1,8 @@
+config.doWrite=False
+config.doAddDistortionModel = False
+config.doDefect = False
+config.doAssembleIsrExposures = False
+config.doBias=True
+config.doDark=True
+config.doFlat=True
+config.doSaturationInterpolation = False
