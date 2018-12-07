ad76cc98753a1ba96c6f3f4224a96483037092d8
jrmullaney <j.mullaney@sheffield.ac.uk>
2018-12-07 20:34:32 +0000

    Moved isr config from processCcd.py to isr.py

--- config/processCcd.py
+++ config/processCcd.py
@@ -1,4 +1,3 @@
-#from lsst.obs.goto.gotoNullIsr import GotoNullIsrTask
 import os.path
 from lsst.utils import getPackageDir
 from lsst.meas.algorithms import ColorLimit
@@ -7,14 +6,7 @@ from lsst.meas.algorithms import LoadIndexedReferenceObjectsTask
 configDir = os.path.join(getPackageDir("obs_goto"), "config")
 
 #ISR:
-config.isr.doWrite=False
-config.isr.doAddDistortionModel = False
-config.isr.doDefect = False
-config.isr.doAssembleIsrExposures = False
-config.isr.doBias=True
-config.isr.doDark=True
-config.isr.doFlat=True
-config.isr.doSaturationInterpolation = False
+config.isr.load(os.path.join(configDir, "isr.py"))
 
 #CharImage:
 #Retarget the characterize image task to my own in gotoCharTask.
