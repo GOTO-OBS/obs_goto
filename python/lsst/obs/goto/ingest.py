4bc8c9ef08d56d8e1a9666cd1d1784c8ef2a4730
jrmullaney <j.mullaney@sheffield.ac.uk>
2023-04-19 18:35:41 +0100

    Attempting to ingest raw files; time formatting needs adjusting

--- python/lsst/obs/goto/ingest.py
+++ python/lsst/obs/goto/ingest.py
@@ -1,63 +1,18 @@
-from lsst.pipe.tasks.ingestCalibs import CalibsParseTask
-#from lsst.pipe.tasks.ingest import IngestTask, ParseTask, IngestArgumentParser
-from lsst.pipe.tasks.ingest import ParseTask
-from astropy.time import Time
-import re
+from lsst.obs.base.ingest import RawFileData
+import lsst.obs.base
+from ._instrument import North1, North2
 
-class GotoCalibsParseTask(CalibsParseTask):
+class GotoRawIngestTask(lsst.obs.base.RawIngestTask):
 
-    def _translateFromCalibId(self, field, md):
-        data = md.get("CALIB_ID")
-        match = re.search(".*%s=(\S+)" % field, data)
-        return match.groups()[0]
+    def extractMetadata(self, filename: str) -> RawFileData:
+        datasets = []
+        fitsData = lsst.afw.fits.Fits(filename.ospath, 'r')
+        fitsData.setHdu(1)
+        header = fitsData.readMetadata()
+        datasets.append(self._calculate_dataset_info(header, filename))
+        instrument = NeCam()
+        FormatterClass = instrument.getRawFormatter(datasets[0].dataId)
 
-    def translate_ccd(self, md):
-        return self._translateFromCalibId("ccd", md)
-    
-    def translate_filter(self, md):
-        return self._translateFromCalibId("filter", md)
-    
-    def translate_calibDate(self, md):
-        return self._translateFromCalibId("calibDate", md)
-    
-class GotoParseTask(ParseTask):
-
-    def translateDate(self, md):
-
-        date = md.get("DATE-OBS")
-        start = date[11:]
-        date = date.strip()[:10]
-        t = Time(date)
-        
-    #If after midnight, set date to date minus 1 day.
-        if int(start.split(":")[0]) < 12:
-            date = Time(t.jd-1, format='jd', out_subfmt='date').iso
-        
-        return date
-     
-    def translateVisit(self, md):
-        visit = md.get("DB-PNT")
-
-        #If no visit number (e.g., flat), revert to the run number.
-        if visit == 'NA':
-            run = md.get("RUN-ID")
-            return int(run.strip('r'))
-        else:
-            return int(visit)
-
-    def translateCcd(self, md):
-        ccd = md.get("INSTRUME")
-        return int(ccd.strip('UT'))
-
-    def translateJd(self, md):
-        date = self.translateDate(md)
-        t = Time(date, format='iso')
-        return int(t.mjd)
-
-    def translateSurvey(self, md):
-        tileName = md.get('TILENAME')
-        event = md.get('EVENT')
-        if (tileName != 'NA') and (event == 'NA'):
-            return 'T'
-        else:
-            return 'F'
+        return RawFileData(datasets=datasets, filename=filename,
+                           FormatterClass=FormatterClass,
+                           instrument=instrument)
