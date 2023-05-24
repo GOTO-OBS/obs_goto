ed642a7b8900a21e8f02da59ad3b58b84732ff97
jrmullaney <j.mullaney@sheffield.ac.uk>
2023-05-24 15:51:02 +0100

    Testing Black

--- python/lsst/obs/goto/forcedPhotVisit.py
+++ python/lsst/obs/goto/forcedPhotVisit.py
@@ -1,42 +1,45 @@
 #!/usr/bin/env python
 
-'''
+"""
 Task to perfom forced photometry on a coadded visit frame (i.e., that
 made up from all exposures from a visit/ccd combination.
 
 Largely defers to ForcedPhotCcd and ForcedPhotImage, but with its own run function
 that allows an exposure to be passed to the function. This feature is useful if one
 wants to include forced photometry in a calling script. It still works, however, if
-called from the command line with --id's. 
-'''
+called from the command line with --id's.
+"""
 
 from lsst.meas.base.forcedPhotCcd import ForcedPhotCcdTask, ForcedPhotCcdConfig
 import lsst.pipe.base
 
+
 class ForcedPhotVisitConfig(ForcedPhotCcdConfig):
     pass
 
-class ForcedPhotVisitTask(ForcedPhotCcdTask):
 
+class ForcedPhotVisitTask(ForcedPhotCcdTask):
     ConfigClass = ForcedPhotVisitConfig
     RunnerClass = lsst.pipe.base.ButlerInitializedTaskRunner
     _DefaultName = "forcedVisitCcd"
     dataPrefix = "visitCoadd_"
-    
+
     def run(self, dataRef, exposure=None, psfCache=None):
-        
         refWcs = self.references.getWcs(dataRef)
         if exposure is None:
             exposure = self.getExposure(dataRef)
-            
+
         refCat = self.fetchReferences(dataRef, exposure)
-        measCat = self.measurement.generateMeasCat(exposure, refCat, refWcs,
-                                                   idFactory=self.makeIdFactory(dataRef))
+        measCat = self.measurement.generateMeasCat(
+            exposure, refCat, refWcs, idFactory=self.makeIdFactory(dataRef)
+        )
         self.log.info("Performing forced measurement on %s" % (dataRef.dataId,))
         self.attachFootprints(measCat, refCat, exposure, refWcs, dataRef)
-        self.measurement.run(measCat, exposure, refCat, refWcs, exposureId=self.getExposureId(dataRef))
+        self.measurement.run(
+            measCat, exposure, refCat, refWcs, exposureId=self.getExposureId(dataRef)
+        )
 
-        '''
+        """
         applyApCorr currently doesn't work; apCorrMap doesn't have a get attribute.
         My suspicion is that an ApCorrMap needs to be created during singleVisitDriver.
         if self.config.doApCorr:
@@ -44,14 +47,14 @@ class ForcedPhotVisitTask(ForcedPhotCcdTask):
                 catalog=measCat,
                 apCorrMap=exposure.getInfo().getApCorrMap()
             )
-        '''
+        """
         self.catalogCalculation.run(measCat)
         self.writeOutput(dataRef, measCat)
 
     def _getConfigName(self):
         return "forcedPhotVisit_config"
-        #return self.dataPrefix + "forcedPhotCcd_config"
+        # return self.dataPrefix + "forcedPhotCcd_config"
+
     def _getMetadataName(self):
         return "forcedPhotVisit_metadata"
-        #return self.dataPrefix + "forcedPhotCcd_metadata"
-
+        # return self.dataPrefix + "forcedPhotCcd_metadata"
